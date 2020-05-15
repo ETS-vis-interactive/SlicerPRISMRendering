d44139275276d3bbf5c7ad7c4b780adaa3cef023
Tiphaine Richard <tiphainejh@hotmail.fr>
2020-05-15 14:41:23 -0400

    outline shader

--- RenduVolumiqueCustomShader/Resources/Shaders/OutlineShader.py
+++ RenduVolumiqueCustomShader/Resources/Shaders/OutlineShader.py
@@ -1,6 +1,7 @@
 from Resources.CustomShader import CustomShader
 
 class OutlineShader(CustomShader):
+  shaderfParams = { 'gradStep' : { 'displayName' : 'Gradient Step', 'min' : 0.0, 'max' : 0.02, 'defaultValue' : 0.005 }}
 
   def __init__(self, shaderPropertyNode):
     CustomShader.__init__(self,shaderPropertyNode)
@@ -15,22 +16,22 @@ class OutlineShader(CustomShader):
     self.shaderProperty.ClearAllFragmentShaderReplacements()
 
     croppingDecCode = """
-    vec4 ComputeGradient( in sampler3D volume, vec3 pos, float gradStep )
+    vec4 ComputeGradient(in sampler3D volume, vec3 pos, float gradStep)
     {
       vec3 g1;
-      g1.x = texture3D( volume, pos + vec3(gradStep,0.0,0.0)  ).x;
-      g1.y = texture3D( volume, pos +  vec3(0.0, gradStep, 0.0) ).x;
-      g1.z = texture3D( volume, pos +  vec3(0.0,0.0, gradStep) ).x;
+      g1.x = texture3D(volume, pos + vec3(gradStep,0.0,0.0) ).x;
+      g1.y = texture3D(volume, pos +  vec3(0.0, gradStep, 0.0)).x;
+      g1.z = texture3D(volume, pos +  vec3(0.0,0.0, gradStep)).x;
       vec3 g2;
-      g2.x = texture3D( volume, pos -  vec3(gradStep,0.0,0.0) ).x;
-      g2.y = texture3D( volume, pos -  vec3(0.0, gradStep, 0.0) ).x;
-      g2.z = texture3D( volume, pos -  vec3(0.0,0.0,gradStep) ).x;
+      g2.x = texture3D(volume, pos -  vec3(gradStep,0.0,0.0)).x;
+      g2.y = texture3D(volume, pos -  vec3(0.0, gradStep, 0.0)).x;
+      g2.z = texture3D(volume, pos -  vec3(0.0,0.0,gradStep)).x;
       vec3 n = g2 - g1;
-      float nLength = length( n );
-      if( nLength > 0.0 )
-          n = normalize( n );
+      float nLength = length(n);
+      if(nLength > 0.0)
+          n = normalize(n);
       else
-          n = vec3( 0.0, 0.0, 0.0 );
+          n = vec3(0.0, 0.0, 0.0);
       vec4 ret;
       ret.rgb = n;
       ret.a = nLength;
@@ -38,8 +39,7 @@ class OutlineShader(CustomShader):
     }
 
     float sampleThreshold = 0.1;
-    float gradStep = 0.005;
-    vec2 step = vec2( 0.001, 0.01 );
+    vec2 step = vec2(0.001, 0.01);
     float virtualAlpha = 0.0;
     """
     self.shaderProperty.AddFragmentShaderReplacement("//VTK::Cropping::Dec", True, croppingDecCode, False)
@@ -52,14 +52,14 @@ class OutlineShader(CustomShader):
       scalar = vec4(scalar.r);
       g_srcColor = vec4(0.0);
       float inAlpha = computeOpacity(scalar);
-      if( inAlpha > sampleThreshold && virtualAlpha < .95 )
+      if(inAlpha > sampleThreshold && virtualAlpha < .95)
       {
-        vec4 n = ComputeGradient( in_volume[0], g_dataPos, gradStep );
-        if( n.a > 0.0 )
+        vec4 n = ComputeGradient(in_volume[0], g_dataPos, gradStep);
+        if(n.a > 0.0)
         {
-          float factor = n.a * ( 1.0 - abs(dot( normalize(g_dirStep), n.rgb )) );
-          float alpha = smoothstep( step.x, step.y , factor );
-          g_srcColor = vec4( 1.0, 1.0, 1.0, alpha );
+          float factor = n.a * (1.0 - abs(dot(normalize(g_dirStep), n.rgb)));
+          float alpha = smoothstep(step.x, step.y , factor);
+          g_srcColor = vec4(1.0, 1.0, 1.0, alpha);
         }
       }
       virtualAlpha += (1-virtualAlpha) * inAlpha;
